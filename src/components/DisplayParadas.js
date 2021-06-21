bf3b268a73ddc32a4188a4bcc44a3eefa5a127a7
Ivan Jimenez Luque <ivanjimluqmallorca@gmail.com>
2021-06-21 16:11:44 +0200

    arreglos varios del componente cabecera (sacado el header de display paradas y borrado lo que sobraba

--- src/components/DisplayParadas.js
+++ src/components/DisplayParadas.js
@@ -1,7 +1,6 @@
-
-export const DisplayParadas = (props) => { 
-   const {routeId, line, destination, time} = props;
-    const paradas = [
+export const DisplayParadas = (props) => {
+  const { routeId, line, destination, time } = props;
+  const paradas = [
     {
       routeId: "0681",
       line: "68",
@@ -25,27 +24,28 @@ export const DisplayParadas = (props) => {
       "t-in-s": 1288,
       destination: "Pl. Catalunya",
       "t-in-min": 21,
-    }
-      ];
-    
+    },
+  ];
 
-    return (
+  return (
     <>
-            <header className="cabecera">
-                <h1>{routeId}</h1>
-                <div className="display">
-                    <div className="bus">
-                    <span className="linea">{line}</span>
-                    <span className="destino">{destination}</span>
-                    <span className="tiempo">10min</span>
-                    </div>
-                </div>
-                {paradas.map((parada) => <h1>{parada.routeId}</h1>)} 
-                {paradas.map((parada) => <span className="linea">{parada.routeId}</span>)}
-                {paradas.map((parada) => <span className="destino">{parada.destination}</span>)}
-                
-                
-            </header>
+      <h1>{routeId}</h1>
+      <div className="display">
+        <div className="bus">
+          <span className="linea">{line}</span>
+          <span className="destino">{destination}</span>
+          <span className="tiempo">10min</span>
+        </div>
+      </div>
+      {paradas.map((parada) => (
+        <h1>{parada.routeId}</h1>
+      ))}
+      {paradas.map((parada) => (
+        <span className="linea">{parada.routeId}</span>
+      ))}
+      {paradas.map((parada) => (
+        <span className="destino">{parada.destination}</span>
+      ))}
     </>
-    );
+  );
 };
