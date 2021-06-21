db4333762208087d2779b5572dc0c5e2c4f8f3fb
federico <federicotiralongo95@gmail.com>
2021-06-21 13:22:41 +0200

    arreglo del Display

--- src/components/Cabecera.js
+++ src/components/Cabecera.js
@@ -1,28 +1,12 @@
 import { TiempoLinea } from "./TiempoLinea";
+import { DisplayParadas } from "./DisplayParadas";
 
 export const Cabecera = (props) => {
   const { paradas, paradaSeleccionada } = props;
   return (
-    <header className="cabecera">
-      <h1>Parada nº 15</h1>
-      <div className="display">
-        <div className="bus">
-          <span className="linea">V16</span>
-          <span className="destino">Universitat</span>
-          <span className="tiempo">10min</span>
-        </div>
-        <div className="bus">
-          <span className="linea">H12</span>
-          <span className="destino">Pla de Palau</span>
-          <span className="tiempo">1min</span>
-        </div>
-        <div className="bus">
-          <span className="linea">32</span>
-          <span className="destino">Barceloneta</span>
-          <span className="tiempo">4min</span>
-        </div>
-      </div>
+    <>
+      <DisplayParadas paradas={paradas}/>
       <TiempoLinea />
-    </header>
+    </>
   );
 };
