f93cf01e4df46ef3844c4f2d8fa94d5372357cd9
Ignasi <i.elgstrom@gmail.com>
2021-06-21 17:33:44 +0200

    getParadas acabado

--- src/components/Cabecera.js
+++ src/components/Cabecera.js
@@ -9,26 +9,31 @@ export const Cabecera = (props) => {
   const { paradas, paradaSeleccionada } = props;
   const [tiempoEspera, setTiempoEspera] = useState("sin tiempo seleccionado");
   const getTiempoEspera = useCallback((paradas, paradaSeleccionada) => {
-    const tiempoEsperaDummy = paradas.find((parada) =>
-      parada.line === paradaSeleccionada ? parada : ""
-    );
+    if (paradas !== undefined && paradas.length > 0) {
+      const tiempoEsperaDummy = paradas.find((parada) =>
+        parada.line === paradaSeleccionada ? parada : ""
+      );
 
-    return tiempoEsperaDummy["text-ca"];
+      return tiempoEsperaDummy !== undefined
+        ? tiempoEsperaDummy["text-ca"]
+        : "No hay parada seleccionada";
+    }
+    return "No hay parada seleccionada";
   }, []);
 
-  useEffect(
-    () => setTiempoEspera(getTiempoEspera(paradas, paradaSeleccionada)),
-    [getTiempoEspera, paradas, paradaSeleccionada]
-  );
+  useEffect(() => {
+    paradas[0].routeId !== "" &&
+      setTiempoEspera(getTiempoEspera(paradas, paradaSeleccionada));
+  }, [getTiempoEspera, paradas, paradaSeleccionada]);
 
   return (
     <header className="cabecera">
-      <DisplayParadas paradas={paradas} />
+      {paradas[0].routeId !== "" && <DisplayParadas paradas={paradas} />}
       <TiempoLinea
         paradaSeleccionada={paradaSeleccionada}
-        tiempoEspera={tiempoEspera}/>
+        tiempoEspera={tiempoEspera}
+      />
     </header>
-
   );
 };
 
